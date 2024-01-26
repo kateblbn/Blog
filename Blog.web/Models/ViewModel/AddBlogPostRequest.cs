0735652275d9a6c37fe5b5be4227c05a42599d1a
Kate <kate.pidoni@gmail.com>
2024-01-26 22:20:21 +0100

    Create HTML blog pattern

---
+++ Blog.web/Models/ViewModel/AddBlogPostRequest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Blog.web.Models.ViewModel
+{
+    public class AddBlogPostRequest
+    {
+        public string Heading { get; set; }
+        public string PageTitle { get; set; }
+        public string Content { get; set; }
+        public string ShortDescription { get; set; }
+        public string FeaturedImageUrl { get; set; }
+        public string UrlHandle { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Author { get; set; }
+        public bool Visible { get; set; }
+
+        //display tags
+        public IEnumerable<SelectListItem> Tags { get; set; }
+    }
+}
