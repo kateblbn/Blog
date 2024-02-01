a70a04fb9a86010f7abf696e0b26d08a8985c58a
Kate <kate.pidoni@gmail.com>
2024-02-01 20:10:09 +0100

    display tags on page

--- Blog.web/Controllers/HomeController.cs
+++ Blog.web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Blog.web.Models;
+using Blog.web.Models.ViewModel;
 using Blog.web.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -9,17 +10,22 @@ namespace Blog.web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IBlogPostRepository blogPostRepository;
+        private readonly ITagRepository tagRepository;
 
-        public HomeController(ILogger<HomeController> logger, IBlogPostRepository blogPostRepository)
+        public HomeController(ILogger<HomeController> logger, 
+            IBlogPostRepository blogPostRepository, ITagRepository tagRepository)
         {
             _logger = logger;
             this.blogPostRepository = blogPostRepository;
+            this.tagRepository = tagRepository;
         }
 
         public async Task<IActionResult> Index()
         {
             var blogPosts = await blogPostRepository.GetAllAsync();
-            return View(blogPosts);
+            var tags = await tagRepository.GetAllAsync();
+            var model = new HomeViewModel { BlogPosts = blogPosts, Tags = tags };   
+            return View(model);
         }
 
         public IActionResult Privacy()
---
+++ Blog.web/Models/ViewModel/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Blog.web.Models.Domain;
+
+namespace Blog.web.Models.ViewModel
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<BlogPost> BlogPosts { get; set; }
+        public IEnumerable<Tag> Tags { get; set; }
+
+    }
+}
